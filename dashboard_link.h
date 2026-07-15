1f41bee4868aa8c100dd8eec860077e682eaf1df
ardraprajan <ardraprajan@gmail.com>
2026-07-15 09:35:38 +0530

    Add files via upload

---
+++ dashboard_link.h
@@ -0,0 +1,10 @@
+MAIN DASHBOARD
+https://io.adafruit.com/ardrap/dashboards/health-monitor
+
+
+FECILITY MANAGEMENT DASHBOARD LINK
+https://io.adafruit.com/ardrap/dashboards/facility-management-dashboard
+
+
+MEDICAL STAFF DASHBOARD
+https://io.adafruit.com/ardrap/dashboards/medical-staff-dashboard
